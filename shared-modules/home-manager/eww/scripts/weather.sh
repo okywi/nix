90b51f9d922d70752ec407eebef32ab15ec50fae
okywi <m.laupheimer@protonmail.com>
2025-06-03 23:01:03 +0200

    changed weather script to work when location unavailable

--- shared-modules/home-manager/eww/scripts/weather.sh
+++ shared-modules/home-manager/eww/scripts/weather.sh
@@ -14,8 +14,16 @@ fi
 (
     flock -n 9 || exit 1
     while [ true ]; do 
-        curl -s 'wttr.in/Leutkirch?format=%t' | awk '{$1=$1};1' > /tmp/weather-temp
-        curl -s 'wttr.in/Leutkirch?format=%c' | awk '{$1=$1};1' > /tmp/weather-icon
+        temp="$(curl -s 'wttr.in/Leutkirch?format=%t' | awk '{$1=$1};1')"
+        if [[ "$temp" == *"Unknown"* ]]; then
+            temp="-°C"
+        fi
+        echo $temp > /tmp/weather-temp
+        icon="$(curl -s 'wttr.in/Leutkirch?format=%c' | awk '{$1=$1};1')"
+        if [[ "$icon" == *"Unknown"* ]]; then
+            icon="-"
+        fi
+        echo $icon > /tmp/weather-icon
         sleep 180
     done
 ) 9>"$LOCKFILE"
