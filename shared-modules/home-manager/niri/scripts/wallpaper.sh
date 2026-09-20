5a7910b071dd5cc656997412c27e780ec2a204e5
okywi <m.laupheimer@protonmail.com>
2026-09-20 17:55:45 +0200

    fix wallpaper.sh, update flake

--- shared-modules/home-manager/niri/scripts/wallpaper.sh
+++ shared-modules/home-manager/niri/scripts/wallpaper.sh
@@ -1,7 +1,7 @@
 if which linux-wallpaperengine >/dev/null 2>&1; then
     pkill linux-wallpaperengine 2>/dev/null; linux-wallpaperengine --screen-root DP-3 --screen-root HDMI-A-1 --fps 30 --no-audio-processing --no-fullscreen-pause 1897834524
 elif which mpvpaper >/dev/null 2>&1; then
-    pkill mpvpaper 2>/dev/null; mpvpaper --auto-pause -o "--loop --no-audio" "*" ~/Pictures/Animated_Wallpapers/aesthetic-cassette-FHD.mp4
+    pkill mpvpaper 2>/dev/null; mpvpaper --auto-pause -o "--loop --no-audio" "*" ~/Nextcloud/Pictures/Animated_Wallpapers/aesthetic-cassette-FHD.mp4
 else
     pkill awww 2>/dev/null
     awww-daemon &
--- shared-modules/home-manager/sway/scripts/wallpaper.sh
+++ shared-modules/home-manager/sway/scripts/wallpaper.sh
@@ -1,7 +1,7 @@
 if which linux-wallpaperengine >/dev/null 2>&1; then
     pkill linux-wallpaper 2>/dev/null; linux-wallpaperengine --screen-root DP-3 --screen-root HDMI-A-1 --fps 30 --no-audio-processing --no-fullscreen-pause 1897834524
 elif which mpvpaper >/dev/null 2>&1; then
-    pkill mpvpaper 2>/dev/null; mpvpaper -o "--loop --no-audio" "*" ~/Pictures/Animated_Wallpapers/suzume.mp4
+    pkill mpvpaper 2>/dev/null; mpvpaper -o "--loop --no-audio" "*" ~/Nextcloud/Pictures/Animated_Wallpapers/suzume.mp4
 else
     pkill awww 2>/dev/null && awww-daemon &
     sleep 0.5
